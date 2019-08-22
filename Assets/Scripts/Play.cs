5919d3b6fd556ecaf50352db0c04a56b6b2f8379
yusronhanan <yusronzain@gmail.com>
2019-08-22 17:14:42 +0700

    next level

--- Assets/Scripts/Play.cs
+++ Assets/Scripts/Play.cs
@@ -11,11 +11,25 @@ public class Play : MonoBehaviour
     public void MainMenu(string infoScene)
     {
         SceneManager.LoadScene(infoScene);
+		Data.prevScene = infoScene;
+
         //Data.infoScene = infoScene;
         //sound.Play();
         //		textText.text = Data.infoScene;
     }
-    
+    public void NextLevel()
+    {
+        if(Data.prevScene == "Level1")
+        {
+            SceneManager.LoadScene("Level2");
+            Data.prevScene = "Level2";
+        } else if (Data.prevScene == "Level2")
+        {
+            SceneManager.LoadScene("Home");
+            Data.prevScene = "Home";
+        }
+    }
+
 
     // Update is called once per frame
     /*void Update()
